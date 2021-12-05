2e990f3bc88f434ce1a54df449dca389f1f1f0f6
tanbin-hasnat-shehab <85411927+tanbin-hasnat-shehab@users.noreply.github.com>
2021-12-05 17:44:59 +0600

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,44 @@ from openpyxl import load_workbook
 import pandas as pd
 from earthquak import EQ
 from sfd import sfd
+from loc_fishing import loc_ph
 
-choices=st.sidebar.selectbox('list of apps',['Earthquak load calculation','wind load calculation','structural analysis of beam,frame,truss'])
+from steel_db import stdb
+from circle_detection import cir
+from cpm_python import cp3
+from moving_load import go
+from sfd import sfd
+
+
+choices=st.sidebar.selectbox('list of apps',['Earthquak load calculation',
+											'wind load calculation',
+											'structural analysis of beam,frame,truss',
+											'aisc steel section database',
+											'circle detection',
+											'critical path method-cpm',
+											'location phising',
+											'moving load analysis'
+											])
 
 
 if choices=='Earthquak load calculation':
 	EQ.Earthquak_load_cal()
 if choices=='structural analysis of beam,frame,truss':
 	sfd.sfd_bmd()
+
+
+
+if choices=='aisc steel section database':
+	stdb.st_database()
+
+if choices=='circle detection':
+	cir.main_detection()
+if choices=='critical path method-cpm':
+	cp3.cpm_main()
+if choices=='location phising':
+	loc_ph.loc_main()
+if choices=='moving load analysis':
+	go.moving_main()
+
 
 	
