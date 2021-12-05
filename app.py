f5bd896c38463bd127c3d4d5d35a1101deb1f90f
tanbin-hasnat-shehab <85411927+tanbin-hasnat-shehab@users.noreply.github.com>
2021-12-05 15:47:01 +0600

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ import streamlit as st
 from openpyxl import load_workbook
 import pandas as pd
 from earthquak import EQ
+from sfd import sfd
 
-choices=st.sidebar.selectbox('list of apps',['Earthquak load calculation','wind load calculation'])
+choices=st.sidebar.selectbox('list of apps',['Earthquak load calculation','wind load calculation','structural analysis of beam,frame,truss'])
 
 
 if choices=='Earthquak load calculation':
 	EQ.Earthquak_load_cal()
+if choices=='structural analysis of beam,frame,truss':
+	sfd.sfd_bmd()
 
 	
