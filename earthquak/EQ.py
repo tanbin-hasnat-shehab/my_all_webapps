7033338a5311e9a5a282d6a8f8de34c1c53746b1
tanbin-hasnat-shehab <85411927+tanbin-hasnat-shehab@users.noreply.github.com>
2021-12-05 22:27:48 +0600

    Update EQ.py

--- earthquak/EQ.py
+++ earthquak/EQ.py
@@ -2,20 +2,27 @@ import streamlit as st
 from openpyxl import load_workbook
 import pandas as pd
 def Earthquak_load_cal():
-
 	
 	wb=load_workbook('earthquak/db.xlsx')
 	
 	sheet=wb['sheet']
 
-	st.header('Earthquak load calculation')
+	st.header("Earthquak load calculation as per BNBC'20")
 	loc_arr=[]
 	for i in range(10,76):
 		loc_arr.append(sheet.cell(row=i,column=1).value)
 	location=st.selectbox('Location',loc_arr)
 	Z=float(sheet.cell(row=loc_arr.index(location)+10, column=2).value)
 	print(f'z is {Z}')
-	
+	if Z==.12:
+		zone=0
+	if Z==.20:
+		zone=1
+	if Z==.28:
+		zone=2
+	if Z==.36:
+		zone=3
+	print(f'zone={zone}')
 	num_o_sto=st.checkbox('input number of storey or input height of structure')
 	if num_o_sto:
 		basement_height=st.text_input('basement height in meter')
@@ -38,13 +45,12 @@ def Earthquak_load_cal():
 
 	occupency_catagory=st.selectbox('Occupency catagory',occupency_arr)
 	if occupency_catagory:
+		print(occupency_catagory)
 		importance_factor=float(sheet.cell(row=occupency_arr.index(occupency_catagory)+161, column=2).value)
-	seismic_force_resisting_system_arr=[]
-	for i in range(189,222):
-		seismic_force_resisting_system_arr.append(sheet.cell(row=i,column=1).value)
-	seismic_force_resisting_system=st.selectbox('Seismic Force-Resisting System',seismic_force_resisting_system_arr)
-	if seismic_force_resisting_system:
-		R=float(sheet.cell(row=seismic_force_resisting_system_arr.index(seismic_force_resisting_system)+189, column=2).value)
+	
+	####
+
+
 	ita=1
 	st.title('Soil report needed for N values')
 	pen_spacing=st.text_input('equi-depth in SPT per penetration','5')
@@ -65,12 +71,66 @@ def Earthquak_load_cal():
 		N=sum_nomi/sum_denomi
 		if N<15:
 			site_class='SD'
+			site_class_no=3
+			seismic_deg_cat_row=178
 		elif N>=15 and N<=50:
 			site_class='SC'
+			seismic_deg_cat_row=177
+			site_class_no=2
 		elif N>50 and N<100:
 			site_class='SB'
+			site_class_no=1
+			seismic_deg_cat_row=176
 		else:
 			site_class='SA'
+			seismic_deg_cat_row=175
+			site_class_no=0
+
+		if occupency_catagory=='IV':
+			seismic_deg_cat_col_start=6
+		else:
+			seismic_deg_cat_col_start=2
+
+		seismic_deg_cat=sheet.cell(row=seismic_deg_cat_row,column=seismic_deg_cat_col_start+zone).value
+		print(seismic_deg_cat)
+		if seismic_deg_cat=='B':
+			seismic_force_resisting_system_suggestion='ordinary seismic_force_resisting_system'
+		if seismic_deg_cat=='C':
+			seismic_force_resisting_system_suggestion='intermediate seismic_force_resisting_system'
+		if seismic_deg_cat=='D':
+			seismic_force_resisting_system_suggestion='special seismic_force_resisting_system'
+		print(seismic_force_resisting_system_suggestion)
+		my_splsh=f'<p style="font-family:Courier; color:Blue; font-size: 20px;">{seismic_force_resisting_system_suggestion}</p>'
+		st.markdown(my_splsh, unsafe_allow_html=True)
+
+
+		seismic_force_resisting_system_arr=[]
+		for i in range(189,222):
+			seismic_force_resisting_system_arr.append(sheet.cell(row=i,column=1).value)
+		seismic_force_resisting_system=st.selectbox('Seismic Force-Resisting System',seismic_force_resisting_system_arr)
+		if seismic_force_resisting_system:
+			R=float(sheet.cell(row=seismic_force_resisting_system_arr.index(seismic_force_resisting_system)+189, column=2).value)
+			system_over_strength_factor=float(sheet.cell(row=seismic_force_resisting_system_arr.index(seismic_force_resisting_system)+189, column=3).value)
+			Deflection_Amplification_Factor=float(sheet.cell(row=seismic_force_resisting_system_arr.index(seismic_force_resisting_system)+189, column=4).value)
+			Ss=float(sheet.cell(row=258,column=2+zone).value)
+			S1=float(sheet.cell(row=259,column=2+zone).value)
+			Fa=float(sheet.cell(row=264+site_class_no,column=2+zone).value)
+			Fv=float(sheet.cell(row=273+site_class_no,column=2+zone).value)
+			
+		
+
+
+
+
+			
+
+
+
+
+
+
+
+
 		for i in range(150,154):
 			if sheet.cell(row=i, column=1).value==site_class:
 				S=float(sheet.cell(row=i, column=2).value)
@@ -103,6 +163,36 @@ def Earthquak_load_cal():
 			if Sa<Sa_min:
 				Sa=Sa_min
 			st.title(f'Base shear is {Sa*100}%')
+
+
+
+
+			to_df2=[]
+			
+			to_df2.append(['Ecc. ratio (All depth)',0.05])
+			to_df2.append(['Ct(ft).x =',m])
+			to_df2.append(['Response modification factor,R',R])
+			to_df2.append(['system over strength,Omega',system_over_strength_factor])
+			to_df2.append(['Deflection amplification ,Cd',Deflection_Amplification_Factor])
+			to_df2.append(['Occupency Importance,I',importance_factor])
+			to_df2.append(['0.2 sec spectral Accel,Ss',Ss])
+			to_df2.append(['1 sec spectral Accel,S1',S1])
+			to_df2.append(['Long period transition period',2])
+			to_df2.append(['Site coeffecient,Fa',Fa])
+			to_df2.append(['Site coeffecient,Fv',Fv])
+			to_df2.append(['zone factor,Z',Z])
+			to_df2.append(['Site class',site_class])
+			to_df2.append(['Seismic design catagory',seismic_deg_cat])
+			to_df2.append(['Seismic force resisting system',seismic_force_resisting_system])
+			#to_df2.append(['',])
+			df2=pd.DataFrame(to_df2,columns=['Name of coeffecient','value'])
+			st.subheader('Co coeffecients needed for etabs 2016 or higher')
+			st.write(df2)
+
+
+
+
+
 			siesmic_weight=st.text_input('seismic weight from stadd or etabbs')
 			if siesmic_weight:
 				base_shear=float(siesmic_weight)*Sa
@@ -111,7 +201,7 @@ def Earthquak_load_cal():
 				if floor_loads_cal:
 					n=st.text_input('no of heights')
 
-					c1,c2=st.columns((1,1))
+					c1,c2=st.beta_columns((1,1))
 					if n:
 						n=int(n)
 						with c1:
